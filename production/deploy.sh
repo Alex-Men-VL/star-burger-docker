d6ac7bd5a51a2c99f3807a2a908ba33275976bf7
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-04-22 22:35:44 +0300

    Improve the deploy script

--- production/deploy.sh
+++ production/deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+docker-compose down
 docker-compose -f ../docker-compose.yaml -f docker-compose.prod.yaml up -d --build
 docker-compose -f ../docker-compose.yaml -f docker-compose.prod.yaml exec web python manage.py migrate
 docker cp star-burger-frontend:/usr/src/app/bundles/ tmp && docker cp tmp/. star-burger-django:/usr/src/app/staticfiles
 rm -r tmp
+
+docker rm star-burger-frontend  # The container has completed its function and has finished
 
 echo "Deploy in production is completed"

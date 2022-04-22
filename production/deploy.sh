551eac35a8dcc358fdfa6868d56ba3d9cd69c196
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-04-22 18:38:00 +0300

    Add a deploy script for production

---
+++ production/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker-compose -f docker-compose.yaml -f production/docker-compose.prod.yaml up -d --build
+docker-compose -f docker-compose.yaml -f production/docker-compose.prod.yaml exec web python manage.py migrate
+docker cp star-burger-frontend:/usr/src/app/bundles/ tmp && docker cp tmp/. star-burger-django:/usr/src/app/staticfiles
+rm -r tmp
+
+echo "Deploy in production is completed"
